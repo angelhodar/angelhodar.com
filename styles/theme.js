ad53e00264462dc812789fdedc33fd1b492c7488
Angel <angelhodar76@gmail.com>
2021-03-04 19:33:09 +0100

    Used local storage initial color mode if exists #5

--- styles/theme.js
+++ styles/theme.js
@@ -1,8 +1,13 @@
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react";
 
-const config = {
-  initialColorMode: "light",
-  useSystemColorMode: false,
+let initialColorMode = "light";
+
+if (typeof window !== "undefined") {
+  // Load saved one if page has previously tweaked theme mode
+  const savedColorMode = localStorage.getItem("chakra-ui-color-mode");
+  initialColorMode = savedColorMode || initialColorMode;
 }
 
-export default extendTheme({ config })
+const config = { initialColorMode, useSystemColorMode: false };
+
+export default extendTheme({ config });
