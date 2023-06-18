365c2144eb6bfb7577328865013d197db1fc4271
Angel <angelhodar76@gmail.com>
2023-06-18 16:10:05 +0200

    Moved to vercel static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
+  output: "static",
   adapter: vercel({
     analytics: true
   })
