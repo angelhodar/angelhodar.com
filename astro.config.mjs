6de51d551877b8eb759b508a2c704d3726e66285
Angel <angelhodar76@gmail.com>
2023-06-18 13:40:21 +0200

    Fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
