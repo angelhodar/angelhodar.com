74133b77a89ddb3e724f5d77d746b2fc5a92bac4
Angel <angelhodar76@gmail.com>
2023-06-18 13:32:20 +0200

    Moved to astro

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/edge";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel({
+    analytics: true
+  })
+});
