bfc343b7911db9ed4af95a2db76752f2f682d3f1
Angel Hodar <angelhodar76@gmail.com>
2024-02-04 11:26:24 +0100

    Updated deps and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [icon(), tailwind()],
   output: "static",
   adapter: vercel({
     webAnalytics: true,
