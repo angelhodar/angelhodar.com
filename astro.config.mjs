585354b31b04cdd3b1ccb3ccda7cb97a783015e2
angelhodar <angelhodar76@gmail.com>
2025-08-05 21:14:29 +0200

    Tailwind v4, dehn and updated deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,23 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import {
   transformerNotationDiff,
   transformerNotationHighlight
 } from '@shikijs/transformers'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind(), mdx()],
+  integrations: [icon(), mdx()],
   output: "static",
   markdown: {
     shikiConfig: {
       theme: "catppuccin-frappe",
       transformers: [transformerNotationDiff(), transformerNotationHighlight()]
     }
+  },
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,4 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
 }
